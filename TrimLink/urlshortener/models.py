6c071809cafdb69f1f5c419d7a9387bece2dacce
Ekansh Goenka <goenka.ekansh@gmail.com>
2024-01-06 16:00:28 +0800

    added logic

--- TrimLink/urlshortener/models.py
+++ TrimLink/urlshortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=20000)
+    short_url = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f'{self.url} - {self.short_url}'
--- TrimLink/urlshortener/views.py
+++ TrimLink/urlshortener/views.py
@@ -1,9 +1,66 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect
+import random
+
+from .models import Url
+
+
+nums = [i for i in range(9)]
+chars = [chr(i) for i in range(65, 91)]
+odds = ['k', 'e', ',', '4', 'a', 'b', 'a', '3', 'p', 'l']
 
 
 def index(request):
+    if request.method == 'POST':
+        url = request.POST['url']
+        short_url = create_shorturl()
+        url = Url(url=url, short_url=short_url)
+        url.save()
+        return render(request, 'urlshortener/show.html', {'url': short_url})
+
     return render(request, 'urlshortener/index.html')
 
 
 def url(request, url_id):
-    return render(request, 'urlshortener/url.html')
+    short_urls = [url.short_url for url in Url.objects.all()]
+
+    if url_id in short_urls:
+        url = Url.objects.get(short_url=url_id)
+        url = url.url
+        try:
+            return redirect(url)
+        except:
+            return Http404()
+
+    return Http404()
+
+
+def url_exists(short_url):
+    short_urls = [url.short_url for url in Url.objects.all()]
+    if short_url in short_urls:
+        return True
+
+    return False
+
+
+def build_shorturl():
+    short_url = ''
+
+    short_url += random.choice(chars)
+    short_url += str(random.choice(nums))
+    short_url += random.choice(odds)
+    short_url += random.choice(chars)
+    short_url += str(random.choice(nums))
+    short_url += random.choice(odds)
+
+    return short_url
+
+
+def create_shorturl():
+    short_url = build_shorturl()
+
+    while url_exists(short_url):
+        short_url = build_shorturl()
+
+    return short_url
